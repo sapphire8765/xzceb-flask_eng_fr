da989509ff9a2311e609c0943165d0845da007f0
sapphire8765 <93414507+sapphire8765@users.noreply.github.com>
2021-11-07 12:38:38 -0400

    Add files via upload

--- final_project/machinetranslation/tests/test_translator.py
+++ final_project/machinetranslation/tests/test_translator.py
@@ -8,21 +8,29 @@ class TestTranslator(unittest.TestCase):
     """ This class contains functions to test the translation between English and French """
     def test_english_to_french(self):
         """
-        This function unit tests the translation from English to French
+        This function tests the translation from English to French
         """
-
-        self.assertEqual(english_to_french(''), 'Nothing to translate')
         self.assertEqual(english_to_french('Hello'), 'Bonjour')
         self.assertNotEqual(english_to_french('Hello'), 'Au revoir')
 
     def test_french_to_english(self):
         """
-        This function unit tests the translation from French to English
+        This function tests the translation from French to English
         """
-
-        self.assertEqual(french_to_english(''), 'Nothing to translate')
         self.assertEqual(french_to_english('Bonjour'), 'Hello')
         self.assertNotEqual(french_to_english('Bonjour'), 'Goodbye')
+
+    def test_null_english_to_french(self):
+        """
+        This function checks if null input was provided
+        """
+        self.assertEqual(english_to_french(''), 'Nothing to translate')
+
+    def test_null_french_to_english(self):
+        """
+        This function checks if null input was provided
+        """
+        self.assertEqual(french_to_english(''), 'Nothing to translate')
 
 if __name__=='__main__':
     unittest.main()
